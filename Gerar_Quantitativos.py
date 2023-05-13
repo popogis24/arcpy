344818ed6684ecbab625e8653cff387e1098729f
anderson <133429641+popogis24@users.noreply.github.com>
2023-05-13 19:18:44 -0300

    Update Gerar_Quantitativos.py

--- Gerar_Quantitativos.py
+++ Gerar_Quantitativos.py
@@ -1,36 +1,43 @@
 import arcpy
 
-# Inputs
-dados_relatorio = arcpy.GetParameterAsText(0)
-area_estudo = arcpy.GetParameterAsText(1)
-pasta_output = arcpy.GetParameterAsText(2)
-tabela_excel = arcpy.GetParameterAsText(3)
-arcpy.env.workspace = dados_relatorio
+    # Inputs
+    camadas = arcpy.GetParameterAsText(0)
+    area_estudo = arcpy.GetParameterAsText(1)
+    pasta_output = arcpy.GetParameterAsText(2)
+    pasta_excel = arcpy.GetParameterAsText(3)
 
-# Loop
-featureclasses = arcpy.ListFeatureClasses()
-for fc in featureclasses:
-    output = str(fc) + '_clipped'
-    clip = arcpy.analysis.Clip(fc, area_estudo, f'{pasta_output}/{output}.shp', "")
+    arcpy.env.workspace = camadas
 
-    # Ferramentas dentro do loop
-    poligono = arcpy.management.AddGeometryAttributes(clip, 'AREA', "", "SQUARE_METERS")
-    ponto = arcpy.management.AddGeometryAttributes(clip, 'POINT_X_Y_Z_M', "", "")
+    # Loop
+    featureclasses = arcpy.ListFeatureClasses()
+    for fc in featureclasses:
+        output = str(fc) + '_clipped'
+        clip = arcpy.analysis.Clip(fc, area_estudo, f'{pasta_output}/{output}.shp', "")
 
-    # Verificar o tipo de feição
-    desc = arcpy.Describe(clip)
-    if desc.shapeType == 'Polygon':
-        poligono
-    elif desc.shapeType == 'Point':
-        ponto
-    else:
-        pass
-    
-    #agora pego o resultado do clip e transformo em tabela
-    nome_xls = str(fc)
-    arcpy.conversion.TableToExcel(clip, nome_xls, "", "")
-    
+        # Ferramentas dentro do loop
+        poligono = arcpy.management.AddGeometryAttributes(clip, 'AREA', "", "SQUARE_METERS")
+        ponto = arcpy.management.AddGeometryAttributes(clip, 'POINT_X_Y_Z_M', "", "")
 
-    
+        # Verificar o tipo de feição
+        desc = arcpy.Describe(clip)
+        if desc.shapeType == 'Polygon':
+            poligono
+        elif desc.shapeType == 'Point':
+            ponto
+        else:
+            pass
+        
+        #agora pego o resultado do clip e transformo em tabela
+        nome_xls = fr'{pasta_excel}/quantitativo_{str(fc)}'
+        trans_tabela = arcpy.conversion.TableToExcel(clip, nome_xls, "", "")
+        if desc.shapeType == 'Polygon' or desc.shapeType == 'Point':
+            trans_tabela
+        else:
+            pass
+        
+        
+
+
+        
 
 
