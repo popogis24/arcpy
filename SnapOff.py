87fc3244610d7d0a35e7484432a07fc17764f07b
anderson <133429641+popogis24@users.noreply.github.com>
2023-08-25 12:10:32 -0300

    Update SnapOff.py

--- SnapOff.py
+++ SnapOff.py
@@ -1,8 +1,23 @@
 import arcpy
 
+arcpy.env.overwriteOutput = True
+
 poligonos = arcpy.GetParameterAsText(0)
 output = arcpy.GetParameterAsText(1)
 
+autoincremento = '''
+rec=0
+def autoIncrement():
+ global rec
+ pStart = 1 #muda para nao iniciar do num 1
+ pInterval = 1 #mude este numero para mudar o intervalo de seq
+ if (rec == 0): 
+  rec = pStart 
+ else: 
+  rec = rec + pInterval 
+ return rec
+ '''
+ 
 #polygon to line codigo completo
 arcpy.PolygonToLine_management(poligonos, "ftl", "IDENTIFY_NEIGHBORS")
 
@@ -15,8 +30,8 @@ arcpy.SelectLayerByAttribute_management("featuretoline_lyr", "NEW_SELECTION", '"
 #crie uma camada temporaria com as linhas selecionadas
 arcpy.MakeFeatureLayer_management("featuretoline_lyr", "featuretoline_lyr2")
 
-#buffer de 10 milimetros nas linhas selecionadas, right, round, all
-arcpy.Buffer_analysis("featuretoline_lyr2", "buffer", "2 Centimeters", "RIGHT", "ROUND", "ALL")
+#buffer de 10 milimetros nas linhas selecionadas, full, round, all, geodesic
+arcpy.Buffer_analysis("featuretoline_lyr2", "buffer", "10 Millimeters", "FULL", "ROUND", "ALL", "", "GEODESIC")
 
 #crie uma camada temporaria com os poligonos
 arcpy.MakeFeatureLayer_management(poligonos, "poligonos_lyr")
@@ -24,6 +39,16 @@ arcpy.MakeFeatureLayer_management(poligonos, "poligonos_lyr")
 #erase dos poligonos com o buffer
 arcpy.Erase_analysis("poligonos_lyr", "buffer", "erase")
 
+#explode multpart to singlepart
+arcpy.MultipartToSinglepart_management("erase", output)
+
+#calculate field com autoincremento como double
+arcpy.CalculateField_management(output, "div", "autoIncrement()", "PYTHON3", autoincremento,"LONG", "")
+
+
+#output
+#with arcpy.EnvManager(outputZFlag="Disabled", outputMFlag="Disabled"):
+#arcpy.CopyFeatures_management("erase", output)
 
 #delete das camadas temporarias
 arcpy.Delete_management("ftl")
